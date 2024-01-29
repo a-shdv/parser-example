61d1c9d6e6fadf3d64cfa7c1476e6f1252b408bd
Anton <shadaev2001@icloud.com>
2024-01-29 17:22:39 +0400

    Update Main.java

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import com.company.eurofilter.OneEuroFilter;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -8,88 +7,97 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
-import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.logging.Logger;
+import java.text.DecimalFormat;
+import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-    static String rawSpeed = "";
-    static double speed = 0.0;
-    static int avgLength = 0;
-    static int avgFilteredLength = 0;
+    static String length;
+    static String lengthSmoothed = "";
 
     public static void main(String[] args) {
-        Logger log = Logger.getLogger(Main.class.getName());
 
-        FileOutputStream outputStream = null;
-        try {
-            outputStream = new FileOutputStream("C:\\Users\\Антон\\Desktop\\avg-0.0.3.xlsx");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet("Sheet1");
-
-        Number number;
-        NumberFormat format = NumberFormat.getInstance(Locale.getDefault());
-
-        Row rowInit = sheet.createRow(0);
-
-        // HEADER: speed
-        Cell cellSpeed = rowInit.createCell(0);
-        cellSpeed.setCellValue("Speed");
-
-        // HEADER: length
-        Cell cellLength = rowInit.createCell(1);
-        cellLength.setCellValue("AVG length");
-
-        // HEADER: filtered length
-        Cell cellFilteredLength = rowInit.createCell(2);
-        cellFilteredLength.setCellValue("AVG filtered length");
-
-        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Антон\\Desktop\\test.txt"))) {
-            String line;
-            int step = 1;
-            OneEuroFilter oef = new OneEuroFilter(20);
-
-            while ((line = br.readLine()) != null) {
-                // parsing
-                rawSpeed = line.split("\t")[0];
-                if (rawSpeed.contains(",")) {
-                    number = format.parse(rawSpeed);
-                    speed = number.doubleValue();
-                } else {
-                    speed = Double.parseDouble(rawSpeed);
-                }
-                avgLength = Integer.parseInt(line.split("\t")[1]);
-                avgFilteredLength = (int) oef.filter(avgLength);
-
-                // speed
-                Row rowNext = sheet.createRow(step);
-                Cell cell1 = rowNext.createCell(0);
-                cell1.setCellValue(speed);
-
-                // average length
-                Cell cell2 = rowNext.createCell(1);
-                cell2.setCellValue(avgLength);
-
-                // filtered average length
-                Cell cell3 = rowNext.createCell(2);
-                cell3.setCellValue(avgFilteredLength);
-
-                log.info("\n{\n" +
-                "\tspeed: " + speed + "\n" +
-                "\tavgLength: " + avgLength + "\n" +
-                "\tavgFilteredLength: " + avgFilteredLength + "\n" +
-                "}\n");
-                workbook.write(outputStream);
-                step++;
+        CompletableFuture<Void> result =  CompletableFuture.runAsync(() -> {
+            FileOutputStream outputStream = null;
+            try {
+                outputStream = new FileOutputStream("C:\\Users\\Антон\\Desktop\\result-1.0.0.xlsx");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
             }
-            workbook.close();
-            outputStream.close();
-        }  catch (Exception e) {
-            log.info(e.getMessage());
-        }
 
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("Sheet1");
+
+            DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+            Row rowInit = sheet.createRow(0);
+
+            Cell cellSpeed = rowInit.createCell(0);
+            Cell cellLength = rowInit.createCell(1);
+            Cell cellLengthSmoothed = rowInit.createCell(2);
+
+            cellSpeed.setCellValue("Speed");
+            cellLength.setCellValue("Length");
+            cellLengthSmoothed.setCellValue("Length smoothed");
+
+            try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Антон\\Desktop\\syslog-1"))) {
+                String line;
+                int step = 1;
+                while ((line = br.readLine()) != null) {
+                    Pattern lengthPattern = Pattern.compile("Length: (\\d+\\.\\d*)");
+                    Pattern lengthSmoothedPattern = Pattern.compile("Length smoothed: (\\d+\\.\\d*)");
+                    Pattern speedPattern = Pattern.compile("Speed\\(mm/s\\): (\\d+\\.\\d*)");
+
+                    // length
+                    Matcher lengthMatcher = lengthPattern.matcher(line);
+                    if (lengthMatcher.find()) {
+                        length = decimalFormat.format(Double.parseDouble(lengthMatcher.group(1)));
+                        System.out.printf("Length: %s\n", length);
+                        continue;
+                    }
+
+                    // smoothed length
+                    Matcher lengthSmoothedMatcher = lengthSmoothedPattern.matcher(line);
+                    if (lengthSmoothedMatcher.find()) {
+                        lengthSmoothed = decimalFormat.format(Double.parseDouble(lengthSmoothedMatcher.group(1)));
+                        System.out.println("Length smoothed: " + lengthSmoothed);
+                        continue;
+                    }
+
+                    // speed
+                    String speed = "";
+                    Matcher speedMatcher = speedPattern.matcher(line);
+                    if (speedMatcher.find()) {
+                        speed = decimalFormat.format(Double.parseDouble(speedMatcher.group(1)));
+                        System.out.println("Speed: " + speed);
+                    } else continue;
+
+                    if (!lengthSmoothed.isEmpty() && !lengthSmoothed.equals("0.0") && !lengthSmoothed.equals("0,0")
+                            && !length.isEmpty() && !length.equals("0.0") && !length.equals("0,0")) {
+                        Row rowNext = sheet.createRow(step);
+                        Cell cell1 = rowNext.createCell(0);
+                        cell1.setCellValue(speed);
+
+                        Cell cell2 = rowNext.createCell(1);
+                        cell2.setCellValue(lengthSmoothed);
+
+                        Cell cell3 = rowNext.createCell(2);
+                        cell3.setCellValue(length);
+                    }
+
+                    workbook.write(outputStream);
+                    step++;
+                }
+                workbook.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println("test");
+        result.join();
     }
 }
